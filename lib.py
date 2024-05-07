36f88f6e69f350a9750a1208bd0d797f8901545f
LHL-source <lhlaw.online@gmail.com>
2024-05-07 15:54:17 +0200

    Add print statement to lib.py

--- lib.py
+++ lib.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/power/<int:x>/<int:y>')
 def power(x, y):
     result = x ** y
+    print("today is 7-5-2024 V1")
     return f"<p>Power of {x} and {y} is {result}</p>"
 
 if __name__ == "__main__":
