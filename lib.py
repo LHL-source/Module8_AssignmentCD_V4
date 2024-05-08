5b5c13eac74a35e9070f9cb945d980213f3bc896
LHL-source <lhlaw.online@gmail.com>
2024-05-08 17:36:45 +0200

    Your commit message

--- lib.py
+++ lib.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/power/<int:x>/<int:y>')
 def power(x, y):
     result = x ** y
-    print("today is 7-5-2024 V1")
+    print("today is 8-5-2024 V2")
     return f"<p>Power of {x} and {y} is {result}</p>"
 
 if __name__ == "__main__":
