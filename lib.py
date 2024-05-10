409083a10e6e939cbdf47482c7617a62ccc781d3
LHL-source <lhlaw.online@gmail.com>
2024-05-10 17:34:32 +0200

    update:secret HOST

--- lib.py
+++ lib.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/power/<int:x>/<int:y>')
 def power(x, y):
     result = x ** y
-    print("today is 8-5-2024 V2")
+    print("today is 10-5-2024 V3")
     return result
     #return f"<p>Power of {x} and {y} is {result}</p>"
 
