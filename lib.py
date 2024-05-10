45166777a8a4ad360ca967e06b6b8de5f9769eb5
LHL-source <lhlaw.online@gmail.com>
2024-05-10 15:34:11 +0200

    lib.py after return changed to result

--- lib.py
+++ lib.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 def power(x, y):
     result = x ** y
     print("today is 8-5-2024 V2")
-    return f"<p>Power of {x} and {y} is {result}</p>"
+    return result
+    #return f"<p>Power of {x} and {y} is {result}</p>"
 
 if __name__ == "__main__":
     app.run()
--- test_lib.py
+++ test_lib.py
@@ -1,5 +1,10 @@
 import lib
 
 def test_power():
-    assert lib.power(2,2) == 4
-    assert lib.power(2,3) == 8
+    # lib.power(2,2) == 4
+    #assert lib.power(2,3) == 8
+
+    assert lib.power(2, 2) == '<p>Power of 2 and 2 is 4</p>'
+    assert lib.power(2, 3) == '<p>Power of 2 and 3 is 8</p>'
+    
+    
