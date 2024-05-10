2354ed0674d4600744726489ca012b3f7fba336f
LHL-source <lhlaw.online@gmail.com>
2024-05-10 15:40:05 +0200

    test_lib.py wijziging assert naar: assert lib.power(2,2) == 4,assert lib.power(2,3) == 8

--- test_lib.py
+++ test_lib.py
@@ -1,10 +1,9 @@
 import lib
 
 def test_power():
-    # lib.power(2,2) == 4
-    #assert lib.power(2,3) == 8
+    assert lib.power(2,2) == 4
+    assert lib.power(2,3) == 8
 
-    assert lib.power(2, 2) == '<p>Power of 2 and 2 is 4</p>'
-    assert lib.power(2, 3) == '<p>Power of 2 and 3 is 8</p>'
+    
     
     
