055b773e3d1d11032b11b99055ae15061bfa153a
LHL-source <lhlaw.online@gmail.com>
2024-05-11 13:27:04 +0200

    Add in workflow.yml Test Gunicorn

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
 from flask import Flask
-#from werkzeug.urls import url_quote
+
 
 app = Flask(__name__)
 
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def power(x, y):
     result = x ** y
     print("today is 10-5-2024 V3")
-    return result
-    #return f"<p>Power of {x} and {y} is {result}</p>"
+    return result #type integer
+    return f"<p>Power of {x} and {y} is {result}</p>"
 
 if __name__ == "__main__":
     app.run()
